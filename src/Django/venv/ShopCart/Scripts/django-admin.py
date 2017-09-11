85a5119fbf0b653b51d7bfa6e936669ecbd2267e
KarateJB <ppippenkimo@gmail.con>
2017-09-11 16:13:00 +0800

    Use pip to restore packages.

--- src/Django/venv/ShopCart/Scripts/django-admin.py
+++ src/Django/venv/ShopCart/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!D:\Works\Source Control\GitHub\KarateJB\Python.Practice\src\Django\venv\ShopCart\Scripts\python.exe
+#!d:\works\source control\github\karatejb\python.practice\src\django\venv\shopcart\scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
