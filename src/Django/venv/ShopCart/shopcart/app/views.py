b547639797573aefdb32d135c549d7c744ad876b
KarateJB <ppippenkimo@gmail.com>
2017-10-10 13:23:20 +0800

    A view data to template sample

--- src/Django/venv/ShopCart/shopcart/app/views.py
+++ src/Django/venv/ShopCart/shopcart/app/views.py
@@ -4,7 +4,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html')
+    car1 = {'Id':1, 'Name': 'Focus', 'Owner'2: 'JB'}
+    car2 = {'Id':2, 'Name': 'Lancer', 'Owner': 'Lily'}
+    car3 = {'Id':3, 'Name': 'Swift', 'Owner': 'leia'}
+    Cars = []
+    Cars.append(car1)
+    Cars.append(car2)
+    Cars.append(car3)
+    
+    context = {'Cars': Cars }
+
+    return render(request, 'index.html', context)
 
 def handler404(request):
     return render(request, '404.html')
