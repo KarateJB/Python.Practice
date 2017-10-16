bcfdb1c4968f57fee57b85161850f78fe452f27f
KarateJB <ppippenkimo@gmail.con>
2017-10-16 14:25:47 +0800

    Add sweetalert before removing product

--- src/Django/venv/ShopCart/shopcart/app/static/scripts/app/components/product-list.js
+++ src/Django/venv/ShopCart/shopcart/app/static/scripts/app/components/product-list.js
@@ -1,11 +1,27 @@
 var prodapp = new Vue({
     el: '#prodapp',
     data: {
-        removeTitle: "刪除"
     },
     methods: {
-        removeAlert: function () {
-            
+        confirmRemove: function (title) {
+            var refs = this.$refs;
+
+            swal({
+                title: 'Are you sure?',
+                text: `\"${title}\" will be removed!`,
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'No',
+                confirmButtonClass: 'btn btn-success',
+                cancelButtonClass: 'btn btn-danger',
+            }).then(function () {
+
+                // refs.removeForm.submit(); //Submit
+
+            }, function (dismiss) {
+
+            })
         }
     }
 })
