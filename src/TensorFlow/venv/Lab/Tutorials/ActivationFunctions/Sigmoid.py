bae9fcd59a077e10d4a0be2593d09fc4f8b628f3
KarateJB <ppippenkimo@gmail.com>
2018-02-17 09:27:27 +0800

    Refactoring

---
+++ src/TensorFlow/venv/Lab/Tutorials/ActivationFunctions/Sigmoid.py
@@ -0,0 +1,39 @@
+"""Sigmoid
+"""
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = tf.placeholder(tf.float32)
+
+with tf.name_scope('Sigmoid'):
+    fx = tf.sigmoid(X)
+    tf.summary.scalar("f(x)", tf.squeeze(fx))
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+
+    # Output graph
+    merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter("log/Sigmoid/", graph = sess.graph)
+    
+    # Run the initializer
+    sess.run(init)
+ 
+    for step in range(-10,11):
+        a = tf.convert_to_tensor(step, dtype=tf.float32)
+        a_r = sess.run([a])
+        print(sess.run(a), sess.run(fx, feed_dict={X: a_r}))
+
+        sess.run(fx, feed_dict={X: a_r})
+        summary = sess.run(merged, feed_dict={X: sess.run([a])})
+        writer.add_summary(summary, step)
+
+# with tf.Session() as sess:
+    
+#     for step in range(-10,11):
+#         X = tf.convert_to_tensor(step, dtype=tf.float32)
+#         # X = tf.random_uniform([1,1], minval=1.0, maxval=3.0, seed=step) //Or use random number
+#         print(sess.run(X), sess.run(tf.sigmoid(X)))
