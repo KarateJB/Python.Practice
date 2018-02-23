d7d777d60d18687ef150c83c8335156b35770ff3
KarateJB <ppippenkimo@gmail.con>
2018-02-23 12:56:32 +0800

    Create random shuffle queue sample

--- src/TensorFlow/venv/Lab/Tutorials/Basic/RandomShuffleQueue.py
+++ src/TensorFlow/venv/Lab/Tutorials/Basic/RandomShuffleQueue.py
@@ -1,16 +1,16 @@
 """
 FIFOQueue
-See https://www.tensorflow.org/api_docs/python/tf/FIFOQueue
+See https://www.tensorflow.org/versions/r1.2/api_docs/python/tf/RandomShuffleQueue
 """
 
 import tensorflow as tf
     
-# shp = [3]
+# shp = [10]
 # dummy_input = tf.placeholder("float", shp)
-dummy_input = tf.random_normal([3], mean=0, stddev=1)
+dummy_input = tf.random_normal([10], mean=0, stddev=1)
 dummy_input = tf.Print(dummy_input, data=[dummy_input], message='Create new dummy inputs: ', summarize=6)
 
-q = tf.FIFOQueue(capacity=3, dtypes=tf.float32)
+q = tf.RandomShuffleQueue(capacity=10, min_after_dequeue=2, dtypes=tf.float32)
 enqueue_op = q.enqueue_many(dummy_input)
 
 data = q.dequeue()
@@ -21,7 +21,7 @@ fg = data + 1
 with tf.Session() as sess:
     # sess.run(enqueue_op, feed_dict={dummy_input: [10.,20.,30.]})
     sess.run(enqueue_op)
-    for i in range(0,3):
+    for i in range(0,8):
         sess.run(fg)
     
     
--- src/TensorFlow/venv/Lab/Tutorials/Sample/FIFOQueue.py
+++ src/TensorFlow/venv/Lab/Tutorials/Sample/FIFOQueue.py
@@ -5,26 +5,24 @@ See https://www.tensorflow.org/api_docs/python/tf/FIFOQueue
 
 import tensorflow as tf
     
-
-shp = [3]
-dummy_input = tf.placeholder("float", shp)
-
-# dummy_input = tf.random_normal([3], mean=0, stddev=1)
-# dummy_input = [-0.373674512,1.18645978,0.696019769]
-dummy_input = tf.Print(dummy_input, data=[dummy_input], message='New dummy inputs have been created: ', summarize=6)
+dummy_input = tf.random_normal([3], mean=0, stddev=1)
+# shp = [3]
+# dummy_input = tf.placeholder("float", shp)
+dummy_input = tf.Print(dummy_input, data=[dummy_input], message='Create new dummy inputs: ', summarize=6)
 
 q = tf.FIFOQueue(capacity=3, dtypes=tf.float32)
 enqueue_op = q.enqueue_many(dummy_input)
 
 data = q.dequeue()
-data = tf.Print(data, data=[q.size(),data], message='This is how many items are left in q: ')
+data = tf.Print(data, data=[q.size(),data], message='How many items are left in queue: ')
 # create a fake graph that we can call upon
 fg = data + 1
 
 with tf.Session() as sess:
     run_option = tf.RunOptions(timeout_in_ms=5000)
     try:
-        sess.run(enqueue_op, feed_dict={dummy_input: [10.,20.,30.]}, options=run_option)
+        # sess.run(enqueue_op, feed_dict={dummy_input: [10.,20.,30.]}, options=run_option)
+        sess.run(enqueue_op, options=run_option)
         for i in range(0,3):
             sess.run(fg, options=run_option)
     except tf.errors.DeadlineExceededError:
