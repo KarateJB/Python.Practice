36d41aae6d72d7eae9614426a206035b973b3419
KarateJB <ppippenkimo@gmail.com>
2018-05-26 11:30:11 +0800

    Load MNIST sample

--- src/DeepLearn/venv/Lab/mnist_load.py
+++ src/DeepLearn/venv/Lab/mnist_load.py
@@ -1,5 +1,10 @@
-import mnist
-# import scipy.misc
+import sys, os
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
 
-images = mnist.train_images()
-# scipy.misc.toimage(scipy.misc.imresize(images[0,:,:] * -1 + 256, 10.))
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
