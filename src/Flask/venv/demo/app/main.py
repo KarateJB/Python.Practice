0174384553e22046438305d9accb735cc4a8f273
KarateJB <ppippenkimo@gmail.com>
2019-01-07 11:02:04 +0800

    Create a broadcast api: send

--- src/Flask/venv/demo/app/main.py
+++ src/Flask/venv/demo/app/main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, send, emit
+import json
+
+
 app = Flask(__name__, template_folder='./templates')
 
 # Websocket setting
@@ -10,24 +13,26 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')    
 
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    """Receive a message and brodcast to all connected clients
+    """
+    jsonobj_content = request.json
+    socketio.emit('server_response',  {'data':str(jsonobj_content)}, broadcast=True)
+    return '', 200
+
 
 # @app.route('/<string:page_name>/')
 # def static_page(page_name):
 #     return render_template('%s.html' % page_name)    
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)    
-
 @socketio.on('connect_event')
 def connected_event(msg):
+    """WebSocket connect event
+    This will trigger responsing a message to client by 
+    """
     print('Received msg: %s', msg)
-    emit('server_response', {'data': msg['data']}, broadcast=True)
-
-@socketio.on('save receipt')
-def handle_json(json):
-    print('Received json: ' + str(json))
-    emit('save-reveipt response', {'isSuccess': 'true'})
+    emit('server_response', {'data': msg['data']})
 
 if __name__=='__main__':
     socketio.run(app)
